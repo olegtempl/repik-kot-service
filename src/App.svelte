261ae972af2b546045b6982ce5285416616193ed
olegtempl <linner_952@mail.ru>
2019-05-29 16:23:41 +0200

    update components

--- src/App.svelte
+++ src/App.svelte
@@ -1,172 +1,207 @@
+<script>
+  // import Filters from "./components/Filters.svelte";
+  import RepetitorCard from "./components/RepetitorCard.svelte";
+
+  import { filter } from "./data/data.js";
+  import { repetitorsInfo } from "./data/data.js";
+  // import ContinuousEmployment from "./filters/ContinuousEmployment.svelte";
+  import SexTeacher from "./components/filters/SexTeacher.svelte";
+  import StatusTeacher from "./components/filters/StatusTeacher.svelte";
+  import EducationDegree from "./components/filters/EducationDegree.svelte";
+  import EducationTeacher from "./components/filters/EducationTeacher.svelte";
+  import GoalEmployment from "./components/filters/GoalEmployment.svelte";
+  import PlaceOfEmployment from "./components/filters/PlaceOfEmployment.svelte";
+  import TypeEmployment from "./components/filters/TypeEmployment.svelte";
+
+  import { fade } from "svelte/transition";
+  let visible = false;
+
+  const showInfoMessage = () => {
+    visible = !visible;
+  };
+
+  export let status = [],
+    sex = [],
+    continuous = [],
+    degree = [],
+    education = [],
+    goal = [],
+    place = [],
+    type = [],
+    name = "",
+    repetitors = [];
+
+  $: repetitors = filter({
+    status,
+    sex,
+    continuous,
+    degree,
+    education,
+    goal,
+    place,
+    type,
+    name
+  });
+</script>
+
+<style>
+
+</style>
+
 <svelte:head>
-	<link rel='stylesheet' href='viktoriya.css'>
+  <link rel="stylesheet" href="viktoriya.css" />
 </svelte:head>
-
-
-
 
 <!-- Start header block-->
 <header>
-	<div class="panel-logo">
-		<div class="name-service-wrap">
-			<div class="name-service">Repik-cat</div>
-		</div>
-		<div class="logo-wrap"><img src="./images/logo.png" alt="Logo Repik-kot project"></div>
-	</div>
-	<div class="promo-block"><img src="./images/header_image.png" alt="Promo image the Repik-kot project" srcset=""></div>
-	<div class="heading-service-wrap">
-		<div class="heading-service">
-			<h2 class="name-service">Repikcat -</h2>
-			<p class="text-center">сэрвіс для пошуку рэпетытараў</p>
-		</div>
-	</div>
+  <div class="panel-logo">
+    <div class="name-service-wrap">
+      <div class="name-service">Repik-cat</div>
+    </div>
+    <div class="logo-wrap">
+      <img src="./images/logo.png" alt="Logo Repik-kot project" />
+    </div>
+  </div>
+  <div class="promo-block">
+    <img
+      src="./images/header_image.png"
+      alt="Promo image the Repik-kot project"
+      srcset="" />
+  </div>
+  <div class="heading-service-wrap">
+    <div class="heading-service">
+      <h2 class="name-service">Repikcat -</h2>
+      <p class="text-center">сэрвіс для пошуку рэпетытараў</p>
+      <p class="text-center text-primary">па Ангельскай мове</p>
+    </div>
+  </div>
 </header>
 <!-- End header block-->
 <!-- Start main block-->
 <main>
-	<!-- Navigation brow-->
-	<div class="content">
-		<div class="section background-dark">
-			<div class="container text-center">
-				<h2 class="text-md text-white">Усе даступныя рэпетытары</h2>
-			</div>
-		</div>
-		<div class="top-repetitors-block">
-			<aside>
-				<div class="filter-panel-wrap">
-					<h2 class="text-md text-center">Фільтр для пошуку рэпетытараў:</h2>
-					<div class="filter-panel">
-						<div class="checkboxes-block">
-							<SexTeacher bind:sex />
-							<StatusTeacher bind:status />
-							<EducationDegree bind:degree />
-							<EducationTeacher bind:education />
-							<GoalEmployment bind:goal />
-							<TypeEmployment bind:type />
-							<PlaceOfEmployment bind:place />
-							<!-- <Filters bind:sex bind:status bind:degree bind:education bind:goal bind:type bind:place /> -->
-						</div>
-					</div>
-				</div>
-			</aside>
+  <!-- Navigation brow-->
+  <div class="content">
+    <div class="section background-dark">
+      <div class="container text-center">
+        <h2 class="text-md text-white">Усе даступныя рэпетытары</h2>
+      </div>
+    </div>
+    bcg_top_repik.svg
+    <div class="top-repetitors-block">
+      <aside>
+        <div class="filter-panel-wrap">
+          <h2 class="text-md text-center">Фільтр для пошуку рэпетытараў:</h2>
+          <div class="filter-panel">
+            <div class="checkboxes-block">
+              <SexTeacher bind:sex />
+              <StatusTeacher bind:status />
+              <EducationDegree bind:degree />
+              <EducationTeacher bind:education />
+              <GoalEmployment bind:goal />
+              <TypeEmployment bind:type />
+              <PlaceOfEmployment bind:place />
+              <!-- <Filters bind:sex bind:status bind:degree bind:education bind:goal bind:type bind:place /> -->
+            </div>
+          </div>
+        </div>
+      </aside>
 
-			<div class="repetitors-preview">
-				<h2 class="text-lg text-black"> Топ 3 рэпетытараў</h2>
-				{#each repetitors as rep, i (rep.personalInfo.id)}
-				{#if i
-				< 3} <RepetitorCard {...rep} />
-				{/if}
-				{:else}
-				<h2 class="text-md text-center text-black">Шкадую, я не змог падабраць такога рэпетытара сяорд топавых</h2>
-				{/each}
+      <div class="repetitors-preview">
+        <h2 class="text-lg text-black">Топ 3 рэпетытараў</h2>
+        {#each repetitors as rep, i}
+          {#if i < 3}
+            <RepetitorCard {...rep} />
+          {/if}
+        {:else}
+          <h2 class="text-md text-center text-black">
+            Шкадую, я не змог падабраць такога рэпетытара сярод топавых
+          </h2>
+        {/each}
 
-			</div>
+      </div>
 
-		</div>
+    </div>
 
+    <!-- Start Sales panel -->
+    <div class="sale-block-wrap">
+      <div class="message-text">
+        <h2 class="text-center text-md ">
+          Пакінь свой email і атрымай зніжку 15%
+        </h2>
+      </div>
+      {#if !visible}
+        <div class="input-wrap">
+          <div class="input">
+            <input id="name" placeholder="ВАШ ЕМЕЙЛ" type="text" />
+          </div>
+          <div class="button-wrap">
+            <button on:click={showInfoMessage}>→</button>
+          </div>
+        </div>
+      {/if}
 
-		<!-- Start Sales panel -->
-		<div class="sale-block-wrap">
-			<div class="message-text">
-				<h2 class="text-center text-md ">Пакінь свой email і атрымай зніжку 15%</h2>
-			</div>
-			<div class="input-wrap">
-				<div class="input">
-					<input id="name" placeholder="ВАШ ЕМЕЙЛ" type="text">
-				</div>
-				<div class="button-wrap">
-					<button>→</button>
-				</div>
-			</div>
-		</div>
-		<div class="repetitors-preview">
-			<h3 class="text-lg text-black text-center"> Усе рэпетытары</h3>
-			{#each repetitors as rep, i (rep.personalInfo.id)}
-			<RepetitorCard {...rep} />
-			{:else}
-			<h2 class="text-md text-center text-black">Шкадую, я не змог падабраць такога рэпетытара</h2>
-			{/each}
-		</div>
-		<!-- End Sales panel -->
-	</div>
+      {#if visible}
+        <!-- <div class="container-sm"> -->
+        <div class="message-bar background-info" transition:fade>
+          <p>
+            Паведамленне на вашу паштовую скрынку адпраўлена, праверце калі
+            ласка пошту
+          </p>
+        </div>
+        <!-- </div> -->
+      {/if}
+    </div>
+    <div class="repetitors-preview">
+      <h3 class="text-lg text-black text-center">Усе рэпетытары</h3>
+      {#each repetitors as rep, i (rep.personalInfo.id)}
+        <RepetitorCard {...rep} />
+      {:else}
+        <h2 class="text-md text-center text-black">
+          Шкадую, я не змог падабраць такога рэпетытара
+        </h2>
+      {/each}
+    </div>
+    <!-- End Sales panel -->
+  </div>
 
-	<!-- 404 page -->
-	<!-- <body class="non"></body> -->
+  <!-- 404 page -->
+  <!-- <body class="non"></body> -->
 </main>
 
-
-
 <footer>
-	<div class="content-footer">
-		<section class="container-sm">
-			<h3 class="text-md text-black text-normal">Сужба патрымкi</h3><a href="404.html">Дапамога</a><a href="404.html">Зварот
-				сродкаў</a><a href="404.html">Бяспека</a>
-		</section>
-		<section class="container-sm">
-			<h3 class="text-md text-black text-normal">Інфармацыя пра сэрвiс</h3><a href="404.html">Пра нас</a><a href="404.html">Вакансіі</a><a
-			 href="404.html">Артыкулы</a><a href="404.html">Мабільны дадатак</a>
-		</section>
-		<section class="container-sm">
-			<h3 class="text-md text-black text-normal">Нашi партнеры</h3><a href="404.html">bivi.by</a><a href="404.html">bear-software.by</a><a
-			 href="404.html">darkdev.by</a>
-		</section>
-	</div>
-	<div class="wrapper">
-		<div class="wrapper-inner ">
-			<div class="text-sm text-white text-normal">Мова: Беларуская</div>
-		</div>
-		<div class="wrapper-inner ">
-			<div class="text-sm text-white text-normal"> Палітыка сайта</div>
-		</div>
-		<div class="wrapper-inner ">
-			<div class="text-sm text-white text-normal">Дамоўленасці</div>
-		</div>
-		<div class="wrapper-inner ">
-			<div class="text-sm text-white text-normal">© 2019 Repik-kot inc.</div>
-		</div>
-	</div>
+  <div class="content-footer">
+    <section class="container-sm">
+      <h3 class="text-md text-black text-normal">Сужба патрымкi</h3>
+      <a href="404.html">Дапамога</a>
+      <a href="404.html">Зварот сродкаў</a>
+      <a href="404.html">Бяспека</a>
+    </section>
+    <section class="container-sm">
+      <h3 class="text-md text-black text-normal">Інфармацыя пра сэрвiс</h3>
+      <a href="404.html">Пра нас</a>
+      <a href="404.html">Вакансіі</a>
+      <a href="404.html">Артыкулы</a>
+      <a href="404.html">Мабільны дадатак</a>
+    </section>
+    <section class="container-sm">
+      <h3 class="text-md text-black text-normal">Нашi партнеры</h3>
+      <a href="404.html">bivi.by</a>
+      <a href="404.html">bear-software.by</a>
+      <a href="404.html">darkdev.by</a>
+    </section>
+  </div>
+  <div class="wrapper">
+    <div class="wrapper-inner ">
+      <div class="text-sm text-white text-normal">Мова: Беларуская</div>
+    </div>
+    <div class="wrapper-inner ">
+      <div class="text-sm text-white text-normal">Палітыка сайта</div>
+    </div>
+    <div class="wrapper-inner ">
+      <div class="text-sm text-white text-normal">Дамоўленасці</div>
+    </div>
+    <div class="wrapper-inner ">
+      <div class="text-sm text-white text-normal">© 2019 Repik-kot inc.</div>
+    </div>
+  </div>
 </footer>
-
-<script>
-	// import Filters from "./components/Filters.svelte";
-	import RepetitorCard from "./components/RepetitorCard.svelte";
-
-	import { filter } from "./data/data.js";
-	// import ContinuousEmployment from "./filters/ContinuousEmployment.svelte";
-	import SexTeacher from "./components/filters/SexTeacher.svelte";
-	import StatusTeacher from "./components/filters/StatusTeacher.svelte";
-	import EducationDegree from "./components/filters/EducationDegree.svelte";
-	import EducationTeacher from "./components/filters/EducationTeacher.svelte";
-	import GoalEmployment from "./components/filters/GoalEmployment.svelte";
-	import PlaceOfEmployment from "./components/filters/PlaceOfEmployment.svelte";
-	import TypeEmployment from "./components/filters/TypeEmployment.svelte";
-
-	export let status = [],
-	  sex = [],
-	  continuous = [],
-	  degree = [],
-	  education = [],
-	  goal = [],
-	  place = [],
-	  type = [],
-	  name = "",
-	  repetitors = [];
-
-	$: repetitors = filter({
-	  status,
-	  sex,
-	  continuous,
-	  degree,
-	  education,
-	  goal,
-	  place,
-	  type,
-	  name
-	});
-</script>
-
-<style>
-</style>
-
-
-
--- src/components/RepetitorCard.svelte
+++ src/components/RepetitorCard.svelte
@@ -1,85 +1,152 @@
-<div class="repetitor-wrap">
-  <div class="avatar-wrap" style="background: url({personalInfo.img}) no-repeat center center;
-  background-size: cover;" srcset=""></div>
-  <div class="repetitor-info-wrap">
-    <h3 class="text-md text-white">{personalInfo.firstName} {personalInfo.lastName}</h3>
-    <div class="info-about-repetitor">
-      <div class="goal">
-        <p>Мэта:</p><span class="text-primary">{ goal.text}</span>
-        <p>Прадметы:</p><span class="text-primary">Ангельская мова</span>
-        <p>Тып занятку:</p><span class="text-primary">{goal.type}</span>
-             </div>
-              <div class="classes">
-        <h4 class="text-sm text-white">Кошт занятку</h4>
-        <div class="spaces-wrap">
-          <div class="space"><span class="text-xs">У вучня:</span>
-            <div class="coast"><span class="text-primary">15 р.</span><span>(60 мiн)</span></div>
-          </div>
-          <div class="space"><span class="text-xs">У рэпетытара:</span>
-            <div class="coast"><span class="text-primary">15 р.</span><span>(60 мiн)</span></div>
-          </div>
-          <div class="space"><span class="text-xs">Дыстанцыйна:</span>
-            <div class="coast"><span class="text-primary">15 р.</span><span>(60 мiн)</span></div>
-          </div>
-        </div>
-        <div class="buttons-panel">
-          <button class="button button-outlined">Дэталёвей ▼</button>
-          <button class="button button-primary">Звязацца</button>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-
 <script>
-	// export let name = "",
-	//   subject = "",
-	//   status = "",
-	//   sex = "";
+  // export let name = "",
+  //   subject = "",
+  //   status = "",
+  //   sex = "";
+  import { fade } from "svelte/transition";
+  let contactsVisible = false,
+    aboutRepikVisible = false,
+    coastVisible = true;
 
-	export let continuousEmployment = "",
-	  educationDegree = "",
-	  placeOfEmployment = "",
-	  goal = {
-	    text: "",
-	    type: "",
-	    coastStudentPlace: "",
-	    coastTeacherPlace: "",
-	    coastRemote: ""
-	  },
-	  personalInfo = {
-	    id: "",
-	    dateOfBirth: "",
-	    status: "",
-	    sex: "",
-	    experience: "",
-	    // Automate generator
-	    otherInfo: "",
-	    firstName: "",
-	    lastName: "",
-	    middleName: "",
-	    avatarPath: "",
-	    city: "",
-	    streetName: "",
-	    country: ""
-	  },
-	  contacts = {
-	    email: "",
-	    phone: ""
-	    // profileFacebook: '',
-	    // profileTwitter: ''
-	  },
-	  reviews = [
-	    {
-	      nameAuthor: "",
-	      text: ""
-	    }
-	  ];
+  const showInfoAboutRepik = () => {
+    aboutRepikVisible = !aboutRepikVisible;
+    coastVisible = !coastVisible;
+  };
+
+  const showContactsInfo = () => {
+    contactsVisible = !contactsVisible;
+    coastVisible = !coastVisible;
+  };
+
+  export let continuousEmployment = "",
+    educationDegree = "",
+    placeOfEmployment = "",
+    discipline = "",
+    goal = {
+      text: "",
+      type: "",
+      coastStudentPlace: "",
+      coastTeacherPlace: "",
+      coastRemote: ""
+    },
+    price = {
+      atTheTeacher: "",
+      atStudent: "",
+      remotely: ""
+    },
+    personalInfo = {
+      id: "",
+      dateOfBirth: "",
+      status: "",
+      sex: "",
+      experience: "",
+      // Automate generator
+      otherInfo: "",
+      firstName: "",
+      lastName: "",
+      middleName: "",
+      avatarPath: "",
+      city: "",
+      streetName: "",
+      country: ""
+    },
+    contacts = {
+      email: "",
+      phone: ""
+      // profileFacebook: '',
+      // profileTwitter: ''
+    },
+    reviews = [
+      {
+        nameAuthor: "",
+        text: ""
+      }
+    ];
 </script>
 
 <style>
 
-
 </style>
+
+<div class="repetitor-wrap">
+  <div
+    class="avatar-wrap"
+    style="background: url({personalInfo.img}) no-repeat center center;
+    background-size: 3.2em;"
+    srcset="" />
+  <div class="repetitor-info-wrap">
+    <h3 class="text-md text-white">
+       {personalInfo.firstName} {personalInfo.lastName}
+    </h3>
+    <div class="info-about-repetitor">
+      <div class="goal">
+        <p>Мэта:</p>
+        <span class="text-primary">{goal.text}</span>
+        <p>Прадметы:</p>
+        <span class="text-primary">{discipline}</span>
+        <p>Тып занятку:</p>
+        <span class="text-primary">{goal.type}</span>
+      </div>
+      <div class="classes">
+        {#if coastVisible}
+          <h4 class="text-sm text-white">Кошт занятку</h4>
+          <div class="spaces-wrap">
+            <div class="space">
+              <span class="text-xs">У вучня:</span>
+              <div class="coast">
+                <span class="text-primary"> {price.atStudent} р.</span>
+                <span>(60 мiн)</span>
+              </div>
+            </div>
+            <div class="space">
+              <span class="text-xs">У рэпетытара:</span>
+              <div class="coast">
+                <span class="text-primary">{price.atTheTeacher} р.</span>
+                <span>(60 мiн)</span>
+              </div>
+            </div>
+            <div class="space">
+              <span class="text-xs">Дыстанцыйна:</span>
+              <div class="coast">
+                <span class="text-primary">{price.remotely} р.</span>
+                <span>(60 мiн)</span>
+              </div>
+            </div>
+          </div>
+        {/if}
+
+        <div class="buttons-panel">
+          <button class="button button-outlined" on:click={showInfoAboutRepik}>
+            Дэталёвей ▼
+          </button>
+          <button class="button button-primary" on:click={showContactsInfo}>
+            Звязацца
+          </button>
+        </div>
+
+        {#if contactsVisible}
+          <div class="container-sm">
+            <div class="message-bar background-success" transition:fade>
+              <p>Паштовая скрынка: {contacts.email}</p>
+              <p>Нумар тэлефона: {contacts.phone}</p>
+            </div>
+          </div>
+        {/if}
+
+        {#if aboutRepikVisible}
+          <div class="container-md">
+            <div class="message-bar background-success" transition:fade>
+              <p>Досвед працы па прадмеце: {personalInfo.experience} год</p>
+              <p>Горад месцазнаходжання: г.{personalInfo.city}</p>
+              <p>Вуліца месцазнаходжання: вул.{personalInfo.streetName}</p>
+              <!-- <p>{personalInfo.country}</p> -->
+              <p>Дадатковая інфармацыя: {personalInfo.otherInfo}</p>
+              <!-- <p>{personalInfo.}</p> -->
+            </div>
+          </div>
+        {/if}
+
+      </div>
+    </div>
+  </div>
+</div>
